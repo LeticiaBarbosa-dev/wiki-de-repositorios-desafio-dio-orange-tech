82af7f6523f383fcdf14f656af5e6e5409ac56bc
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-12-22 19:46:57 -0200

    feat(ItemRepo/Button/Input): Renderiza o componente ItemRepo e adiciona propriedades nos componentes de Button e Input

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,9 @@
-export function Button() {
+interface ButtonProps {
+    onClick: () => void;
+}
+export function Button({onClick}: ButtonProps) {
     return(
-        <button className="bg-#FAFAFA border border-[#FAFAFA] rounded-2xl h-[62px] w-[80%] m-5 hover:bg-[]#FAFAFA40 cursor-pointer">
+        <button onClick={onClick} className="bg-#FAFAFA border border-[#FAFAFA] rounded-2xl h-[62px] w-[80%] m-5 hover:bg-[]#FAFAFA40 cursor-pointer">
             Buscar
         </button>
     )
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,11 @@
-export function Input() {
+interface InputProps {
+    value: any;
+    onChange: any;
+}
+export function Input({value, onChange}: InputProps) {
     return (
         <div className="border border-[#FAFAFA] rounded-2xl h-[62px] w-[80%] m-5">
-            <input type="text" className="bg-transparent border-0 w-[100%] h-[62px] py-0 px-5 text-white outline-none text-xl" /> 
+            <input value={value} onChange={onChange} type="text" className="bg-transparent border-0 w-[100%] h-[62px] py-0 px-5 text-white outline-none text-xl" /> 
         </div>
     )
 }
