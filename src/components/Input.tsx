59d4bd9c1df4fdc25399ae80c2045e8fde1a0d1e
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-12-22 18:42:15 -0200

    feat(input): cria componente de input

---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+export function Input() {
+    return (
+        <div className="border border-[#FAFAFA] rounded-2xl h-[62px] w-[80%] m-5">
+            <input type="text" className="bg-transparent border-0 w-[100%] h-[62px] py-0 px-5 text-white outline-none text-xl" /> 
+        </div>
+    )
+}
