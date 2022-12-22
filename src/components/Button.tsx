de299706dcdce1d04d16309312efb1fe4208cb62
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-12-22 19:04:13 -0200

    feat(button): cria componente de botão

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+export function Button() {
+    return(
+        <button className="bg-#FAFAFA border border-[#FAFAFA] rounded-2xl h-[62px] w-[80%] m-5 hover:bg-[]#FAFAFA40 cursor-pointer">
+            Buscar
+        </button>
+    )
+}
